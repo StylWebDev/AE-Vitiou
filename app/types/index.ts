17c2634f10337011f1bad7db1f0427a3893c1904
StylWebDev <kostasstilianou@gmail.com>
2026-07-23 08:55:29 +0300

    feat: Player type

--- app/types/index.ts
+++ app/types/index.ts
@@ -1,7 +1,18 @@
+export type BoolInt = 0 | 1
+
 export interface HTTPMethods {
   GET: 'GET',
   POST: 'POST',
   PUT: 'PUT',
   DELETE: 'DELETE'
   PATCH: 'PATCH'
+}
+
+export interface Player {
+  id: number;
+  name: string;
+  number: number;
+  pos: 'GK' | 'DEF' | 'MID' | 'EXT' | 'FWD';
+  isCaptain: BoolInt;
+  createdAt: string;
 }
